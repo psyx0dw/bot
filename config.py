243854fdbeba510b006feb123cbd26e3b1cfb985
psyx0dw <jule0505@bk.ru>
2025-10-02 01:21:32 +0300

    Удалить все файлы из репозитория

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-BOT_TOKEN = "8047185520:AAG0c-Ihm1mbKb7AsP5IJhQoHYJz1spIFNE"
-ADMIN_GROUP_ID = -1002981223017   # id группы с админами
-DB_PATH = "database.db"           # путь к SQLite базе
-
-# настройки бонусной системы
-BONUS_PERCENT = 0.05      # 5% баллами от суммы заказа
-MAX_DISCOUNT = 0.15       # максимальная скидка 15%
-REFERRAL_BONUS = 100      # бонусные баллы за приглашение друга
